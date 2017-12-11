3fdd72a9ee114de46d8cf79c1b99e3951fca2e30
Jasonbroker <zhiye1@hotmail.com>
2017-12-11 19:49:00 +0800

    change

---
+++ SFHFKeychainUtils/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNSFHFKeychainUtils
+//
+//  Created by sunny on 17/1/6.
+//  Copyright © 2017年 51talk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SFHFKeychainUtils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SNSFHFKeychainUtils
+//
+//  Created by sunny on 17/1/6.
+//  Copyright © 2017年 51talk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
